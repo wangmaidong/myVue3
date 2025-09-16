b1d73c676d5f71336cc1d69259df23e2487f3358
wanglei <wanglei@unittec.com>
2025-09-16 18:54:13 +0800

    feat: 解决更新bug

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { activeEffect,trackEffect,triggerEffects } from "./effect"
+import { activeEffect, trackEffect, triggerEffects } from "./effect"
 import { toReactive } from "./reactive"
 import { createDep } from "./reactiveEffect"
 export function ref(value) {
@@ -21,36 +21,36 @@ class RefImpl {
         return this._value
     }
     set value(newVal) {
-        if(newVal !== this.rawValue) {
+        if (newVal !== this.rawValue) {
             this.rawValue = newVal
             this._value = newVal
             triggerRefValue(this)
         }
-    }   
+    }
 }
 
 export function trackRefValue(ref) {
-    if(activeEffect) {
-        ref.dep = createDep( () => {
+    if (activeEffect) {
+        ref.dep = ref.dep || createDep(() => {
             ref.dep = undefined
-        } , 'undefined')
+        }, 'undefined')
         trackEffect(activeEffect, ref.dep)
     }
 }
 
 export function triggerRefValue(ref) {
     let dep = ref.dep
-    if(dep) {
+    if (dep) {
         triggerEffects(dep)
     }
 }
 
-export function toRef(object,key) {
-    return new ObjectRefImpl(object,key)
+export function toRef(object, key) {
+    return new ObjectRefImpl(object, key)
 }
 class ObjectRefImpl {
     public __v_isRef = true
-    constructor(public _object,public _key) {}
+    constructor(public _object, public _key) { }
     get value() {
         return this._object[this._key]
     }
@@ -61,27 +61,27 @@ class ObjectRefImpl {
 
 export function toRefs(object) {
     const ret = {}
-    for(const key in object) {
-        ret[key] = toRef(object,key)
+    for (const key in object) {
+        ret[key] = toRef(object, key)
     }
     return ret
- }
+}
 
- export function proxyRefs(objectWithRefs) {
+export function proxyRefs(objectWithRefs) {
     return new Proxy(objectWithRefs, {
-        get(target,key,receiver) {
-            let v = Reflect.get(target,key,receiver)
+        get(target, key, receiver) {
+            let v = Reflect.get(target, key, receiver)
             return v.__v_isRef ? v.value : v
         },
-        set(target, key ,value, receiver)  {
+        set(target, key, value, receiver) {
             const oldValue = target[key]
-            if(oldValue.__v_isRef) {
+            if (oldValue.__v_isRef) {
                 oldValue.value = value
                 return true
             } else {
-                return Reflect.set(target,key,value,receiver)
+                return Reflect.set(target, key, value, receiver)
             }
         }
     })
-    
- }
+
+}
